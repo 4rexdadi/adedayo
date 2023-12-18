61954a32d847100d83867a4fe7f22f1fd4f22421
Adedayo Aturu <dayoaturu45@gmail.com>
2023-12-18 01:35:07 +0100

    Update HeroSection.tsx

--- src/components/dom/heroSection/HeroSection.tsx
+++ src/components/dom/heroSection/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection: FC<HeroSectionProps> = () => {
             a software developer from Lagos, Nigeria focused on creating and occasionally designing
             exceptional, and engaging digital experiences.
           </span>
-          <span>I don't identify myself with a specific technology; I use the best one for the job. Whether it's JavaScript, TypeScript, Python, or C#, I adapt to the needs of the task at hand.</span>
+          <span>I don&#39;t identify myself with a specific technology; I use the best one for the job. Whether it&#39;s JavaScript, TypeScript, Python, or C#, I adapt to the needs of the task at hand.</span>
         </p>
       </h1>
 
