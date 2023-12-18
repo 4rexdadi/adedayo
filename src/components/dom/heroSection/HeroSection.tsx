dac0332d3e977ccafeac23ee1b2503817ce41da6
Adedayo Aturu <dayoaturu45@gmail.com>
2023-12-18 23:11:38 +0100

    Dragger

--- src/components/dom/heroSection/HeroSection.tsx
+++ src/components/dom/heroSection/HeroSection.tsx
@@ -1,5 +1,8 @@
 // Imports
 import { FC } from "react";
+import { LeftArrow, RightArrow } from "../../../../public/assets/svg/Svg";
+import cx from "../../../utils";
+import Dragger from "../../subComponent/dragger/Dragger";
 import style from "./heroSectionStyle.module.scss";
 
 interface HeroSectionProps {}
@@ -14,18 +17,41 @@ const HeroSection: FC<HeroSectionProps> = () => {
         </p>
 
         <p className={style.heroAbout}>
-          <span>a software developer from Lagos, Nigeria, focused on creating and occasionally designing exceptional and engaging digital experiences. Check out my résumé for relevant experience.</span>
-          <span>I don&#39;t identify myself with a specific technology; I use the best one for the job. Whether it&#39;s JavaScript, TypeScript, Python, or C#, I adapt to the needs of the task at hand.</span>
+          <span>
+            A <span>software developer</span> from{" "}
+            <a href="google.com">Lagos, Nigeria</a>, focused on creating and
+            occasionally designing exceptional and engaging digital experiences.
+            <br />
+            Check out my <a href="google.com">résumé</a> for relevant
+            experience.
+          </span>
+          <span>
+            I don&#39;t identify myself with a specific technology; <br /> I use
+            the best one for the job. <br /> Whether it&#39;s JavaScript,
+            TypeScript, Python, or C#, <br />I adapt to the needs of the task at
+            hand.
+          </span>
         </p>
       </h1>
 
-      <div className={style.project}>
-        <div style={{ background: "red" }} />
-        <div style={{ background: "green" }} />
-        <div style={{ background: "pink" }} />
-        <div style={{ background: "blue" }} />
-        <div style={{ background: "orange" }} />
+      <div className={style.projectTitle}>
+        <p>Selected works</p>
+
+        <span>
+          <LeftArrow className="" />
+          <RightArrow className="" />
+        </span>
       </div>
+
+      <Dragger className={cx("dragger")} friction={0.95}>
+        <div className={style.project}>
+          <div className={style.projectC} style={{ background: "red" }} />
+          <div className={style.projectC} style={{ background: "green" }} />
+          <div className={style.projectC} style={{ background: "pink" }} />
+          <div className={style.projectC} style={{ background: "blue" }} />
+          <div className={style.projectC} style={{ background: "orange" }} />
+        </div>
+      </Dragger>
     </section>
   );
 };
