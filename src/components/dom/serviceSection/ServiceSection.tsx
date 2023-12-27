056f64766b7cba1a3005d6ae82951579d93d1a8d
Adedayo Aturu <dayoaturu45@gmail.com>
2023-12-27 21:34:42 +0100

    Added service page

--- src/components/dom/serviceSection/ServiceSection.tsx
+++ src/components/dom/serviceSection/ServiceSection.tsx
@@ -4,7 +4,21 @@ import style from "./serviceSectionStyle.module.scss";
 interface ServiceSectionProps {}
 
 const ServiceSection: FC<ServiceSectionProps> = () => {
-  return <div className={style.serviceContainer}>ServiceSection</div>;
+  return (
+    <div className={style.serviceContainer}>
+      <div className={style.serviceCard}>
+        <div className={style.serviceTop}>
+          <p>services</p>
+        </div>
+
+        <div className={style.serviceBottom}>
+          <div>frontend</div>
+          <div>backend</div>
+          <div>frontend</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceSection;
