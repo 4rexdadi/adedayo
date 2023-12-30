642d44505bf365df47c63b71c8e8b7dd8747c791
Adedayo Aturu <dayoaturu45@gmail.com>
2023-12-30 03:08:14 +0100

    Project section bug fix

--- src/components/dom/projectSection/ProjectSection.tsx
+++ src/components/dom/projectSection/ProjectSection.tsx
@@ -1,4 +1,7 @@
-import { FC } from "react";
+"use client";
+
+import { gsap } from "gsap";
+import { FC, useEffect, useState } from "react";
 import { LeftArrow, RightArrow } from "../../../../public/assets/svg/Svg";
 import Dragger from "../../subComponent/dragger/Dragger";
 import style from "./projectSectionStyle.module.scss";
@@ -6,6 +9,20 @@ import style from "./projectSectionStyle.module.scss";
 interface ProjectSectionProps {}
 
 const ProjectSection: FC<ProjectSectionProps> = () => {
+  const [clickedProject, setClickedProject] = useState<EventTarget>();
+
+  useEffect(() => {
+    if (!clickedProject) return;
+
+    gsap.to(clickedProject, {
+      // width: "100%",
+      // height: "100vh",
+      // position: "fixed",
+      // top: 0,
+      // left: 0,
+    });
+  }, [clickedProject]);
+
   return (
     <section className={style.projectContainer}>
       <div className={style.projectHeader}>
@@ -18,7 +35,7 @@ const ProjectSection: FC<ProjectSectionProps> = () => {
       </div>
 
       <Dragger
-        // onStaticClick={(e) => setClickedProject(e)}
+        onStaticClick={(e) => setClickedProject(e)}
         className="dragger"
         friction={0.95}
       >
--- src/components/dom/serviceSection/ServiceSection.tsx
+++ src/components/dom/serviceSection/ServiceSection.tsx
@@ -1,12 +1,65 @@
-import { FC } from "react";
+"use client";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { FC, useEffect } from "react";
+import cx from "../../../utils";
 import style from "./serviceSectionStyle.module.scss";
 
 interface ServiceSectionProps {}
 
 const ServiceSection: FC<ServiceSectionProps> = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".serviceContainer",
+        start: "top top",
+        end: "2500px",
+        markers: true,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    tl.to(".serviceCard", {
+      translateY: "-25%",
+      rotateZ: "-10deg",
+      ease: "none",
+    })
+      .to(".serviceCard", {
+        translateX: "70%",
+        rotateZ: "0",
+        scale: "2",
+        ease: "none",
+      })
+      .to(".serviceCard", {
+        scale: "2",
+        translateX: "0%",
+        ease: "none",
+      })
+      .to(".serviceCard", {
+        scale: "2",
+        translateX: "-70%",
+        ease: "none",
+      })
+      .to(".serviceCard", {
+        scale: "0",
+        translateX: "0%",
+        translateY: "0%",
+        ease: "none",
+      });
+
+    return () => {
+      tl.kill();
+    };
+  }, []);
+
   return (
-    <div className={style.serviceContainer}>
-      <div className={style.serviceCard}>
+    <div className={cx(style.serviceContainer, "serviceContainer")}>
+      <div className={cx(style.serviceCard, "serviceCard")}>
         <div className={style.serviceTop}>
           <p>services</p>
         </div>
