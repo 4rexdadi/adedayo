7730a92a8afc584fc2574b7c5b1d6ab6b97491cf
Adedayo Aturu <dayoaturu45@gmail.com>
2023-12-30 16:33:18 +0100

    Update constant.tsx

--- src/data/constant.tsx
+++ src/data/constant.tsx
@@ -58,28 +58,6 @@ export const jobData: Work = [
       },
     ],
   },
-  {
-    id: 1,
-    company: "Upstatement",
-    companyWebsite: "google.com",
-    role: "Lead Engineer",
-    date: {
-      start: "May 2018",
-      end: "Present",
-    },
-    accomplishments: [
-      {
-        id: 1,
-        point:
-          "Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more",
-      },
-      {
-        id: 3,
-        point:
-          "Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders",
-      },
-    ],
-  },
 ];
 
 export default { projectData, jobData };
