c32450370902c28599b3a41d6fba3a2f08a2bb1c
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-10 17:15:04 +0100

    Update useIsTouchDevice.tsx

--- src/hooks/useIsTouchDevice.tsx
+++ src/hooks/useIsTouchDevice.tsx
@@ -1,48 +1,33 @@
 "use client";
 
 // Imports
-import { useCallback, useState } from "react";
-import useLayoutEffect from "./useLayoutEffect";
+import { useEffect, useState } from 'react';
 
-const useIsTouchDevice = () => {
-  // isTouchDevice will always be true or false depending if its a touch device or not
-  const [isTouchDevice, setIsTouchDevice] = useState<boolean | null>(null);
+const useIsTouchDevice = (): boolean => {
+  const [isTouchDevice, setIsTouchDevice] = useState<boolean>(false);
 
-  const checkIfTouchDevice = useCallback(() => {
-    // if Pointer Events are supported, just check maxTouchPoints
-    if (window.PointerEvent && "maxTouchPoints" in navigator) {
-      if (navigator.maxTouchPoints > 0) {
-        setIsTouchDevice(true);
-      } else setIsTouchDevice(false);
-
-      if ("ontouchstart" in window) {
-        setIsTouchDevice(true);
-      } else setIsTouchDevice(false);
-    }
-
-    // check for any-pointer:coarse which mostly means touchscreen
-    if (
-      window.matchMedia &&
-      window.matchMedia("(any-pointer:coarse)").matches
-    ) {
+  useEffect(() => {
+    const handleTouchStart = () => {
       setIsTouchDevice(true);
+      window.removeEventListener('touchstart', handleTouchStart);
+    };
 
-      // last resort - check for exposed touch events API / event handler
-    } else if (window.TouchEvent || "ontouchstart" in window) {
-      setIsTouchDevice(true);
-    } else setIsTouchDevice(false);
-  }, []);
+    const checkTouchCapability = () => {
+      const alternativeCheck = ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0);
 
-  useLayoutEffect(() => {
-    // call checkIfTouchDevice on ever client resize to update values
-    checkIfTouchDevice();
-    window.addEventListener("resize", checkIfTouchDevice);
+      setIsTouchDevice(alternativeCheck);
+      if (!alternativeCheck) {
+        window.addEventListener('touchstart', handleTouchStart);
+      }
+    };
+
+    checkTouchCapability();
 
     return () => {
-      // cleanup
-      window.removeEventListener("resize", checkIfTouchDevice);
+      window.removeEventListener('touchstart', handleTouchStart);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return isTouchDevice;
