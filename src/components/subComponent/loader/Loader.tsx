ddf1241d6b6934946d1e0e882f953b31d99e3ed3
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-22 16:37:34 +0100

    Update Loader.tsx

--- src/components/subComponent/loader/Loader.tsx
+++ src/components/subComponent/loader/Loader.tsx
@@ -4,31 +4,11 @@
 import { FC } from "react";
 import style from "./loaderStyle.module.scss";
 
-interface LoaderProps {}
-interface HexBrickProps {
-  type: string;
-}
-
-const HexBrick: React.FC<HexBrickProps> = ({ type }) => (
-  <div className={`${style['hex-brick']} ${style[type]}`} />
-);
-
-const GelRow: React.FC<{ column: number }> = ({ column }) => (
-  <div className={`${style.gel} ${style[`c${column}`]} ${style.r3}`}>
-    <HexBrick type="h1" />
-    <HexBrick type="h2" />
-    <HexBrick type="h3" />
-  </div>
-);
-
 const Loader: FC<LoaderProps> = () => {
-  const columns = Array.from({ length: 12 }, (_, index) => index + 28);
-
+  
   return (
     <div className={`${style.loaderContainer} loaderContainer`}>
-      {columns.map((column) => (
-        <GelRow key={column} column={column} />
-      ))}
+     <div className={style.loader}>Loading</div>
     </div>
   );
 };
