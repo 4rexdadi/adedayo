0db1d7130ec6cda3a82f17b68f9eb44ac30bb13e
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-22 16:39:29 +0100

    Update Loader.tsx

--- src/components/subComponent/loader/Loader.tsx
+++ src/components/subComponent/loader/Loader.tsx
@@ -4,8 +4,10 @@
 import { FC } from "react";
 import style from "./loaderStyle.module.scss";
 
+interface LoaderProps{}
+
 const Loader: FC<LoaderProps> = () => {
-  
+
   return (
     <div className={`${style.loaderContainer} loaderContainer`}>
      <div className={style.loader}>Loading</div>
