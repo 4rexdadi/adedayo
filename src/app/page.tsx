37a191de3807a0fa07ab6d570b46a7d8947c9cfd
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-23 14:53:30 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,31 @@
 import { FC, Suspense } from "react";
 import dynamic from "next/dynamic"
 // import ContactSection from "../components/dom/contactSection/ContactSection";
-import ExperienceSection from "../components/dom/experienceSection/ExperienceSection";
+// import ExperienceSection from "../components/dom/experienceSection/ExperienceSection";
 import HeaderSection from "../components/dom/headerSection/HeaderSection";
-import HeroSection from "../components/dom/heroSection/HeroSection";
-import ProjectSection from "../components/dom/projectSection/ProjectSection";
-import ServicesSection from "../components/dom/servicesSection/ServicesSection";
+// import HeroSection from "../components/dom/heroSection/HeroSection";
+// import ProjectSection from "../components/dom/projectSection/ProjectSection";
+// import ServicesSection from "../components/dom/servicesSection/ServicesSection";
 
 const Home: FC = (): JSX.Element => {
 const ContactSection = dynamic(
   () => import("../components/dom/contactSection/ContactSection")
+);
+
+const ExperienceSection = dynamic(
+  () => import("../components/dom/experienceSection/ExperienceSection")
+);
+
+const ProjectSection = dynamic(
+  () => import("../components/dom/projectSection/ProjectSection")
+);
+
+const ServicesSection = dynamic(
+  () => import("../components/dom/servicesSection/ServicesSection")
+);
+
+const HeroSection = dynamic(
+  () => import("../components/dom/heroSection/HeroSection")
 );
 
   return (
