b0facc9a367c41f9f515641b4833a2cbbc9e993c
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-24 06:24:14 +0100

    Update SingleProject.tsx

--- src/components/subComponent/singleProject/SingleProject.tsx
+++ src/components/subComponent/singleProject/SingleProject.tsx
@@ -8,6 +8,9 @@ interface SingleProjectProps {
 const SingleProject: FC<SingleProjectProps> = ({ clicked }): JSX.Element => {
   return (
     <div
+      style={{
+      transform: clicked?.id ? "translate(-50%, 50%)" : "translate(-50%, 100%)"
+      }}
       className={style.singleProjectContainer}
     >{`Project${clicked?.id}`}</div>
   );
