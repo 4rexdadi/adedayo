f600565169e2110ab425cff6eefe36e1017542d2
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-25 01:07:12 +0100

    Update ScrollContainer.tsx

--- src/components/subComponent/scrollContainer/ScrollContainer.tsx
+++ src/components/subComponent/scrollContainer/ScrollContainer.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ReactLenis } from "@studio-freight/react-lenis";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useRef, useEffect} from "react";
+inport { gsap } from "gsap"
 
 interface ScrollContainerProps {
   children: ReactNode;
@@ -9,8 +10,25 @@ interface ScrollContainerProps {
 }
 
 const ScrollContainer: FC<ScrollContainerProps> = ({ children, root }) => {
+  const lenisRef = useRef()
+  
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+  
+    gsap.ticker.add(update)
+  
+    return () => {
+      gsap.ticker.remove(update)
+    }
+  })
+  
+  
   return (
     <ReactLenis
+      ref={lenisRef} 
+      autoRaf={false}
       root={root}
       options={{
         lerp: 0.1,
