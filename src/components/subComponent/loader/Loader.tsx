c5d46a643d37c1dd7ea5c572a8e8703ff900f43f
Adedayo Aturu <dayoaturu45@gmail.com>
2024-01-27 11:32:08 +0100

    Update Loader.tsx

--- src/components/subComponent/loader/Loader.tsx
+++ src/components/subComponent/loader/Loader.tsx
@@ -2,13 +2,23 @@
 
 // import
 import { useProgress } from "@react-three/drei";
-import { FC } from "react";
+import { FC, useEffect} from "react";
 import style from "./loaderStyle.module.scss";
 
 interface LoaderProps {}
 
 const Loader: FC<LoaderProps> = () => {
   const { progress } = useProgress();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Disable & Enable Scrolling
+    if (progress < 100) {
+      dispatch(setOverflow(false));
+    } else dispatch(setOverflow(true));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [progress]);
+
 
   return progress === 100 ? null : (
     <div className={`${style.loaderContainer} loaderContainer`}>
