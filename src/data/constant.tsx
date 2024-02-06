ec58bdabac118d4deceb7057891791a42e338661
Adedayo Aturu <dayoaturu45@gmail.com>
2024-02-06 23:22:08 +0100

    Update constant.tsx

--- src/data/constant.tsx
+++ src/data/constant.tsx
@@ -18,7 +18,7 @@ export type Work = {
 
 export type Project = {
   id: number;
-  date: number;
+  date: number | string;
   title: string;
   introTitle: string;
   liveSite: string;
@@ -38,14 +38,14 @@ export type Project = {
 export const projectData: Project[] = [
   {
     id: 0,
-    date: 2021,
-    title: "Forex dashboard",
-    introTitle: "demo project",
-    liveSite: "google.com",
+    date: "Jul 2023 - Present",
+    title: "AfroTada",
+    introTitle: "Illuminating Africa - The AfroTada Journey",
+    liveSite: "https://www.afrotada.com",
     linkName: "check out the website",
-    role: "software developer",
-    gitHub: "github.com",
-    technologies: ["react", "css", "next", "python"],
+    role: "Software Developer - Frontend Developerr",
+    gitHub: "",
+    technologies: ["React", "Tailwind CSS", "Redux", "Framer Motion", "Axios", "Quill js"],
     video: "google.com",
     mainImage: Img,
     images: [
@@ -63,21 +63,22 @@ export const projectData: Project[] = [
       },
     ],
     description: [
-      "would be the kind of project I take on: challenging, new, and, of course, fun. The initial pitch was simple: let's build a video-only dating app (Tinder meets TikTok)",
-      "Our collaboration was nothing like I had ever experienced before. They pushed me hard to explore uncharted paths that usually led to a better solution to the problem we were trying to solve",
-      "This remains one of the best projects I've been lucky enough to participate in. I've learned a lot, experienced a lot, failed a lot, and had some of the best product design thinking with Farb and Brian. 10/10 would recommend it 😄",
+      "Overview: AfroTada is more than just a platform; it’s a movement aimed at preserving and celebrating the rich tapestry of African culture, history, and knowledge. By providing a digital space for authentic stories, traditions, and information, AfroTada serves as a beacon of African consciousness, bridging the gap between the past, present, and future.",
+      "Challenges: Building AfroTada from the ground up presented numerous challenges, particularly as the sole frontend developer tasked with creating dynamic features such as the interactive map and dashboard system, which weren’t too hard. The tricky part was developing the custom text editor, which initially I suggested outsourcing. However, our lead engineer, who handles the backend, encouraged me to tackle it myself, and I embraced the challenge. The journey to find the right tool for the text editor led to the discovery of Quill.js, which, despite its steep learning curve, my easy adaptability to new technologies made the integration unnoticed. Quill.js was like the base, so I built the extra features needed for our platform and styled accordingly to match the design.",
+      "Learning: Throughout the AfroTada journey, I acquired proficiency in Tailwind CSS and mastered the art of crafting a sophisticated text editor enriched with custom interactions. Moreover, this project sparked my curiosity in C#, driven by the fast performance of our backend built with ASP.Net Core. This project also expanded my knowledge beyond just tech stuff, also granting me insights into business strategies and the entrepreneurial ecosystem.",
+      "This experience not only enhanced technical skills but also fostered a deeper understanding of the importance of perseverance and adaptability in software development.",
     ],
   },
   {
     id: 1,
-    date: 2021,
-    title: "Forex dashboard",
-    introTitle: "demo project",
+    date: "Sep 2022 - Present",
+    title: "Forex Dashboard",
+    introTitle: "Navigating Markets with Precision through the Forex Dashboard Experience",
     liveSite: "google.com",
-    linkName: "check out the website",
-    role: "software developer",
+    linkName: "try out the demo",
+    role: "Software Developer - Full Stack Web Application",
     gitHub: "github.com",
-    technologies: ["react", "css", "next", "python"],
+    technologies: ["React", "Node", "Express", "SASS", "Redux", "Framer Motion", "Apex Charts", "Mongo DB"],
     video: "google.com",
     mainImage: Img,
     images: [
@@ -95,9 +96,11 @@ export const projectData: Project[] = [
       },
     ],
     description: [
-      "would be the kind of project I take on: challenging, new, and, of course, fun. The initial pitch was simple: let's build a video-only dating app (Tinder meets TikTok)",
-      "Our collaboration was nothing like I had ever experienced before. They pushed me hard to explore uncharted paths that usually led to a better solution to the problem we were trying to solve",
-      "This remains one of the best projects I've been lucky enough to participate in. I've learned a lot, experienced a lot, failed a lot, and had some of the best product design thinking with Farb and Brian. 10/10 would recommend it 😄",
+      "Overview: The Forex Dashboard provides a visual representation of your trading accounts using complex statistical formulas. It implements chart types like pie charts, bar charts, tables, trend lines, etc., to help users identify historical patterns from trade activities, enabling more informed trading/investment decisions.",
+      "Challenges: The original idea was to create a system where users could use their investor login credentials from MT4/MT5 to onboard directly, allowing the dashboard access to users' trading histories and data. However, it turned out to be too expensive. So, I switched gears to a more pragmatic approach, opting to use a free third-party API provided by MyFxbook, which came with its own set of issues and limitations. The solution works, but there's a bit of regret in not having the ideal setup 😞.",
+      "Learning: This project marked my foray into full-stack development. As mentioned, I don't identify with a particular tool. This is a typical example because I had to learn Node.js and Express.js within a short period to use them as a backend solution for this project.",
+      "Despite the hiccups, the project has grown and become a reliable tool for my trading community. It's used almost daily by a few of my forex trading friends. It serves as a reminder that progress often involves overcoming bumps in the road.",
+      "Open Source: The project is openly accessible on GitHub, fostering collaboration and encouraging others to contribute to its evolution.",
     ],
   },
   {
