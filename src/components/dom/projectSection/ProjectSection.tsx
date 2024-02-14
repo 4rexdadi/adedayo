0c8bd188580f4d6820049f136f2f4467287119b0
Adedayo Aturu <dayoaturu45@gmail.com>
2024-02-14 23:33:26 +0100

    fix

--- src/components/dom/projectSection/ProjectSection.tsx
+++ src/components/dom/projectSection/ProjectSection.tsx
@@ -1,22 +1,44 @@
 "use client";
 
 import Image from "next/image";
-import { FC, useState } from "react";
-import { FreeMode } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { FC, useCallback, useRef, useState } from "react";
 import { projectData } from "../../../data/constant";
 import SingleProject from "../../subComponent/singleProject/SingleProject";
 
 // Import styles
 import "swiper/css";
 import "swiper/css/free-mode";
+import Dragger, { OnFrameType } from "../../subComponent/dragger/Dragger";
 import style from "./projectSectionStyle.module.scss";
 
 interface ProjectSectionProps {}
 
 const ProjectSection: FC<ProjectSectionProps> = () => {
-  const [clickedProject, setClickedProject] = useState<number | null>(null);
+  const [clickedProject, setClickedProject] = useState("");
   const [isClicked, setIsClicked] = useState(0);
+  const innerRefArr = Array.from({ length: projectData.length }, () =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useRef<HTMLImageElement>(null)
+  );
+  const outerRefArr = Array.from({ length: projectData.length }, () =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useRef<HTMLLIElement>(null)
+  );
+
+  const onFrame = useCallback(
+    (frame: OnFrameType) => {
+      // bypass Reacts render method to perform frequent style updates, similar concept to React Spring
+      const parallaxFactor = -10;
+      innerRefArr.forEach((ref, i) => {
+        if (outerRefArr.length === i + 1) return;
+
+        const transformX =
+          (frame.x + outerRefArr[i].current!.offsetLeft) / parallaxFactor;
+        ref.current!.style.transform = `translateX(${transformX}px)`;
+      });
+    },
+    [innerRefArr, outerRefArr]
+  );
 
   return (
     <section className={style.projectSection}>
@@ -24,7 +46,55 @@ const ProjectSection: FC<ProjectSectionProps> = () => {
         <p>Selected works</p>
       </div>
 
-      <Swiper
+      <Dragger
+        onFrame={onFrame}
+        onStaticClick={(e) => {
+          setClickedProject(e.id);
+          if (e.id) {
+            setIsClicked((prev) => prev + 1);
+          }
+        }}
+        className="dragger"
+        friction={0.95}
+      >
+        <ul className={style.projectContainer}>
+          {projectData.map((project, i) => {
+            return (
+              <li
+                key={project.id}
+                id={`${project.id}`}
+                className={style.projectCard}
+                ref={outerRefArr[i]}
+              >
+                <div id={`${project.id}`}>
+                  <div id={`${project.id}`} className={style.projectCardImg}>
+                    <Image
+                      alt={project.title}
+                      src={project.mainImage}
+                      draggable={false}
+                      ref={innerRefArr[i]}
+                      id={`${project.id}`}
+                      // width={350}
+                      // height={250}
+                    />
+                  </div>
+
+                  <div id={`${project.id}`} className={style.projectCardInfo}>
+                    <p>{project.mainRole}</p>
+                    <p>{project.date}</p>
+                  </div>
+
+                  <p id={`${project.id}`} className={style.projectCardTitle}>
+                    {project.title}
+                  </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </Dragger>
+
+      {/* <Swiper
         slidesPerView="auto"
         spaceBetween={30}
         freeMode
@@ -62,12 +132,14 @@ const ProjectSection: FC<ProjectSectionProps> = () => {
             </SwiperSlide>
           );
         })}
-      </Swiper>
+      </Swiper> */}
 
       <SingleProject
         isClicked={isClicked}
         clickedProject={
-          projectData.filter((projects) => projects.id === clickedProject)[0]
+          projectData.filter(
+            (projects) => projects.id.toString() === clickedProject
+          )[0]
         }
       />
     </section>
@@ -75,77 +147,3 @@ const ProjectSection: FC<ProjectSectionProps> = () => {
 };
 
 export default ProjectSection;
-
-// const innerRefArr = Array.from({ length: projectData.length }, () =>
-//   // eslint-disable-next-line react-hooks/rules-of-hooks
-//   useRef<HTMLImageElement>(null)
-// );
-// const outerRefArr = Array.from({ length: projectData.length }, () =>
-//   // eslint-disable-next-line react-hooks/rules-of-hooks
-//   useRef<HTMLLIElement>(null)
-// );
-
-// const onFrame = useCallback(
-//   (frame: OnFrameType) => {
-//     // bypass Reacts render method to perform frequent style updates, similar concept to React Spring
-//     const parallaxFactor = -10;
-//     innerRefArr.forEach((ref, i) => {
-//       if (outerRefArr.length === i + 1) return;
-
-//       const transformX =
-//         (frame.x + outerRefArr[i].current!.offsetLeft) / parallaxFactor;
-//       ref.current!.style.transform = `translateX(${transformX}px)`;
-//     });
-//   },
-//   [innerRefArr, outerRefArr]
-// );
-
-// <Dragger
-//         onFrame={onFrame}
-//         onStaticClick={(e) => {
-//           setClickedProject(e.id);
-//           if (e.id) {
-//             setIsClicked((prev) => prev + 1);
-//           }
-//         }}
-//         className="dragger"
-//         friction={0.95}
-//       >
-//         <ul className={style.projects}>
-//           {projectData.map((project, i) => {
-//             return (
-//               <li
-//                 key={project.id}
-//                 id={`${project.id}`}
-//                 className={style.projectCard}
-//                 ref={outerRefArr[i]}
-//               >
-//                 <div id={`${project.id}`}>
-//                   <div
-//                     id={`${project.id}`}
-//                     className={style.projectCardImgContainer}
-//                   >
-//                     <Image
-//                       alt={project.title}
-//                       src={project.mainImage}
-//                       draggable={false}
-//                       ref={innerRefArr[i]}
-//                       // width={350}
-//                       // height={250}
-//                     />
-//                   </div>
-
-//                   <div id={`${project.id}`} className={style.projectCardInfo}>
-//                     <p>{project.role}</p>
-//                     <p>{project.date}</p>
-//                   </div>
-
-//                   <p id={`${project.id}`} className={style.projectCardTitle}>
-//                     {project.title}
-//                   </p>
-//                 </div>
-//               </li>
-//             );
-//           })}
-//         </ul>
-//       </Dragger>
