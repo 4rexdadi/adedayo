267188baaaa3567b3a63fe289069c03181381d82
Adedayo Aturu <dayoaturu45@gmail.com>
2024-02-18 15:48:12 +0100

    Update SingleProject.tsx

--- src/components/subComponent/singleProject/SingleProject.tsx
+++ src/components/subComponent/singleProject/SingleProject.tsx
@@ -86,7 +86,7 @@ const SingleProject: FC<SingleProjectProps> = ({
 
           {clickedProject && (
             <div className={style.singleProjectMainScreen}>
-              <video muted loop preload="none" autoPlay playsInline controls={false}>
+              <video muted loop preload="none" autoPlay="autoplay" playsInLine="playsinline" controls={false}>
                 <source src={clickedProject.video} type="video/mp4" />
                 <track label="English" />
               </video>
