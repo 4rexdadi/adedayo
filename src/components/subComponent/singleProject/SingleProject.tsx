6c822faf7296b42f11eb0cb373aff1145ef28779
Adedayo Aturu <dayoaturu45@gmail.com>
2024-02-26 20:54:49 +0100

    Update SingleProject.tsx

--- src/components/subComponent/singleProject/SingleProject.tsx
+++ src/components/subComponent/singleProject/SingleProject.tsx
@@ -47,7 +47,7 @@ const SingleProject: FC<SingleProjectProps> = ({
     >
       <div className={cx(style.singleProjectHeader, "mainContainer")}>
        <a
-         //target="_blank"
+         // target="_blank"
           href={clickedProject?.liveSite}
           className={cx(style.headerTitle, "heading")}
         >
@@ -126,7 +126,7 @@ const SingleProject: FC<SingleProjectProps> = ({
                 <div className={style.projectLinks}>
                   <a
                     href={clickedProject?.liveSite}
-                    //target="_blank"
+                    // target="_blank"
                   >
                     <span>{clickedProject?.linkName}</span>
                     <RightArrow className={style.rightArrowSvg} />
@@ -135,7 +135,7 @@ const SingleProject: FC<SingleProjectProps> = ({
                   {clickedProject?.gitHub && (
                     <a
                       href={clickedProject?.gitHub}
-                      //target="_blank"
+                      // target="_blank"
                     >
                       <GitHub className={style.gitHubSvg} />
                     </a>
